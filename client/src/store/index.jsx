5f2dadf01e4f7b97e077ace2e1362808c90ed559
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-09-30 12:04:32 +0300

    asdf

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice'; // Import your user slice reducer
+import userReducer from './slices/userSlice'; // Import your user slice 
+import cartReducer from './slices/cartSlice';
+import orderReducer from './slices/orderSlice';
+
 
 const store = configureStore({
   reducer: {
-    user: userReducer, // Only include user reducer for now
+    user: userReducer, // Only include user reducer for 
+    // cart: cartReducer,
+    order: orderReducer,
     // product: productReducer, // You can add product reducer later when needed
   },
   // You can add middleware here if needed
