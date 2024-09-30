c987b30ad6ddd3ffee4161dd3df488cf5fb80e05
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-09-30 16:15:49 +0300

    bara bim bara boom

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice'; // Import your user slice 
 import cartReducer from './slices/cartSlice';
 import orderReducer from './slices/orderSlice';
+import productReducer from './slices/productSlice';
 
 
 const store = configureStore({
@@ -9,7 +10,7 @@ const store = configureStore({
     user: userReducer, // Only include user reducer for 
     // cart: cartReducer,
     order: orderReducer,
-    // product: productReducer, // You can add product reducer later when needed
+    product: productReducer, // You can add product reducer later when needed
   },
   // You can add middleware here if needed
 });
