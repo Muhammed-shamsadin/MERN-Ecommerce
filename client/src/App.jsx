f3c236256228a5d571b148b18e9f26f9366814de
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-10-01 16:32:52 +0300

    Added Protected Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,41 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home'; // Adjust based on your folder structure
+import Home from './pages/Home'; 
 import Login from './pages/Login';
 import Register from './pages/Register';
-import ProductList from './pages/ProductList'; // Example for product listing
-import ProductDetailPage from './pages/ProductDetailPage'; // Example for product details
-import CartPage from './pages/CartPage'; // Import CartPage
-// import CheckoutPage from './pages/Checkout'; // Import CheckoutPage
+import ProductList from './pages/ProductList'; 
+import ProductDetailPage from './pages/ProductDetailPage'; 
+import CartPage from './pages/CartPage'; 
+import CheckoutPage from './pages/Checkout'; 
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
-          <Routes>                         
+          <Routes>                          
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/products" element={<ProductList />} />
-            <Route path="/products/:id" element={<ProductDetailPage />} />
-            <Route path="/cart" element={<CartPage />} /> {/* Add Cart route */}
-            {/* <Route path="/checkout" element={<Checkout />} /> Add Checkout route */}
+            
+            <Route path="/products/:id" element={
+              <ProtectedRoute>
+                <ProductDetailPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/cart" element={
+              <ProtectedRoute>
+                <CartPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/checkout" element={
+              <ProtectedRoute>
+                <CheckoutPage />
+              </ProtectedRoute>
+            } />
           </Routes>
         </main>
       </div>
