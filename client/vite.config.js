b09bd67f4714fb1a6dd8895a547e1b972cd6fb13
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-10-02 10:05:56 +0300

    afdadsf

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/admin': {
+                target: 'http://localhost:5000', // Your backend server
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
