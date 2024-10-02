c2ba02cc0e1be2f40fa8223884087c99d057ca59
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-10-02 10:06:18 +0300

    afdadsf

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,15 @@
-// config/db.js
-const mongoose = require('mongoose');
+// db.mjs
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected Successfully!');
+        console.log('MongoDB connected successfully!');
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+// Export the connectDB function as the default export
+export default connectDB;
