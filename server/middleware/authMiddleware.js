79f7b7afc369d7921eae4f03503280bdb249ff84
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-10-08 13:56:38 +0300

    started

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/User'); // Ensure the path is correct
 
 const protect = async (req, res, next) => {
     let token;
@@ -28,15 +29,4 @@ const protect = async (req, res, next) => {
 };
 
 
-const admin = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
-        next();
-    } else {
-        res.status(401).json({ message: 'Not authorized as an admin' });
-    }
-};
-
-export { 
-    protect, 
-    admin,
- };
+module.exports = { protect }; 
